085b7d0588d1fc3abd36a4e0fd5f12cc36fee98b
p5eudoDjnn <alexander.havers@gmail.com>
2022-09-13 18:44:50 -0400

    add potions to inventory functions and pass test TDD

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -64,3 +64,30 @@ test("subtract health", () => {
 
   expect(player.health).toBe(0);
 });
+
+test("get player attack value", () => {
+  const player = new Player("Dave");
+  player.strength = 10;
+
+  expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+  expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test("adds potion to inventory", () => {
+  const player = new Player("Dave");
+  const oldCount = player.inventory.length;
+
+  player.addPotion(new Potion());
+
+  expect(player.inventory.length).toBeGreaterThanOrEqual(oldCount);
+});
+
+test("uses potion in inventory", () => {
+  const player = new Player("Dave");
+  player.inventory = [new Potion(), new Potion(), new Potion()];
+  const oldCount = player.inventory.length;
+
+  player.usePotion(1);
+
+  expect(player.inventory.length).toBeLessThan(oldCount);
+});
